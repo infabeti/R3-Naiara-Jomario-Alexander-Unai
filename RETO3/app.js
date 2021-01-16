e958e1f1ab5f167151bec2151156f6d7adcbfb56
Unai Santa <unai.santamariape@elorrieta-errekamari.com>
2021-01-16 13:52:47 +0100

    correciones menores como 'bares.html', antes 'Bares.html', código de navegación comentado en index.js

--- RETO3/app.js
+++ RETO3/app.js
@@ -1,34 +1,3 @@
-/* const http = require('http');
-const fs = require('fs');
-const index = require('./index.js');
-const puerto = 4500;
-
-const server = http.createServer(function(peticion, respuesta) {
-	respuesta.writeHead(200, { 'Content-Type': 'text/html'});
-	
-	
-
-	fs.readFile('index.html', function(error, datos){
-		if(error) {
-			respuesta.writeHead(404);
-			respuesta.write('Error al cargar index.js')
-		}
-		else {
-			respuesta.write(datos);
-		}
-		respuesta.end();
-	})
-});
-
-server.listen(puerto, function(error){
-	if(error) {
-		console.log('se ha producido un error');
-	}
-	else {
-		console.log('escuchando en: ' + puerto);
-		console.log(index.mensaje(1));
-	}
-}); */
 const express = require('express')
 const app = express();
 const path = require('path');
@@ -37,6 +6,7 @@ const puerto = 4500;
 
 app.use(express.static('public'));
 
+//Rutas
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '//public//index.html'));
 });
@@ -45,15 +15,17 @@ app.get('/bares', (req, res) => {
   res.sendFile(path.join(__dirname + '//public//bares.html'));
 });
 
-app.get('/restaurantes', (req, res) => {
-  res.sendFile(path.join(__dirname + '//public//restaurantes.html'));
-});
-
 app.get('/cafeterias', (req, res) => {
 	res.sendFile(path.join(__dirname + '//public//cafeterias.html'));
 });
 
+app.get('/restaurantes', (req, res) => {
+  res.sendFile(path.join(__dirname + '//public//restaurantes.html'));
+});
+
+
+
 app.listen(puerto, () => {
   console.log('escuchando en: ' + puerto)
-  console.log(index.mensaje(1));
+  console.log('¡Todo en orden!');
 });
--- RETO3/public/index.js
+++ RETO3/public/index.js
@@ -7,6 +7,29 @@ function botonCafes(){
 function botonRestaurantes(){
 
 } */
+/* $(document).ready(function(){
+	function eleccionSitio(){
+		var id = $(this).attr("id");
+		var url = ""+id+".html";
+		$(this).attr("href", url);
+		
+/* 		document.getElementById(id).onclick = function () {
+        var url = ""+id+".html";
+		$(this).attr("href") = url;
+		window.location.href(url); 
+	}
+	
+	
+/* 	if (id == "bares"){
+		location.href = "bares.html";
+	}
+	else if (id == "cafeterias"){
+		location.href = "cafeterias.html";
+	}
+	else if(id == "restaurantes"){
+		location.href = "restaurantes.html";
+	} 
+}); */
 function mensaje(num){
 	if (num==1){
 		return 'hola, todo en orden'
