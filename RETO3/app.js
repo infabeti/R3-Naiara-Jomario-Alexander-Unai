452c2c0c57ade353a28f95e5b584588cd61d56a9
jomariosantana <jomario.santanaan@elorrieta-errekamari.com>
2021-01-18 14:35:36 +0100

    pruebas con la API de localizacion

--- RETO3/app.js
+++ RETO3/app.js
@@ -4,26 +4,24 @@ const path = require('path');
 const index = require('./public/index.js');
 const puerto = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('public',{extensions:['html']}));
 
 //Rutas
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '//public//index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname + '//public//index.html'));
+// });
 
-app.get('/bares', (req, res) => {
-  res.sendFile(path.join(__dirname + '//public//bares.html'));
-});
+// app.get('/bares', (req, res) => {
+//   res.sendFile(path.join(__dirname + '//public//bares.html'));
+// });
 
-app.get('/cafeterias', (req, res) => {
-	res.sendFile(path.join(__dirname + '//public//cafeterias.html'));
-});
+// app.get('/cafeterias', (req, res) => {
+// 	res.sendFile(path.join(__dirname + '//public//cafeterias.html'));
+// });
 
-app.get('/restaurantes', (req, res) => {
-  res.sendFile(path.join(__dirname + '//public//restaurantes.html'));
-});
-
-
+// app.get('/restaurantes', (req, res) => {
+//   res.sendFile(path.join(__dirname + '//public//restaurantes.html'));
+// });
 
 app.listen(puerto, () => {
   console.log('escuchando en: ' + puerto)
