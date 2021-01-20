a9455d5fea7e0bfe3d84f0ae139df793ab662389
jomariosantana <jomario.santanaan@elorrieta-errekamari.com>
2021-01-20 13:48:53 +0100

    definido errores en app.js

--- RETO3/app.js
+++ RETO3/app.js
@@ -7,23 +7,31 @@ const puerto = 3000;
 app.use(express.static('public',{extensions:['html']}));
 
 //Rutas
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname + '//public//index.html'));
-// });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '//public//index.html'),function(error){
+    if(error){
+      res.status(404).send("<h2>error 404: No existe o no se encuentra el archivo index.html</h2>");
+    }
+  });
+});
 
 // app.get('/bares', (req, res) => {
-//   res.sendFile(path.join(__dirname + '//public//bares.html'));
-// });
-
-// app.get('/cafeterias', (req, res) => {
-// 	res.sendFile(path.join(__dirname + '//public//cafeterias.html'));
-// });
-
-// app.get('/restaurantes', (req, res) => {
-//   res.sendFile(path.join(__dirname + '//public//restaurantes.html'));
-// });
-
-app.listen(puerto, () => {
-  console.log('escuchando en: ' + puerto)
-  console.log('¡Todo en orden!');
+  //   res.sendFile(path.join(__dirname + '//public//bares.html'));
+  // });
+  
+  // app.get('/cafeterias', (req, res) => {
+    // 	res.sendFile(path.join(__dirname + '//public//cafeterias.html'));
+    // });
+    
+    // app.get('/restaurantes', (req, res) => {
+      //   res.sendFile(path.join(__dirname + '//public//restaurantes.html'));
+      // });
+      
+app.listen(puerto, function(error){
+  if(error){
+    console.log('Se ha producido un error al iniciar el servidor');
+  }else{
+    console.log('Escuchando en: ' + puerto);
+    console.log('¡Todo en orden!');
+  }
 });
