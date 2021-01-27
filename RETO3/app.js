de6dac45e2c55302606dbd532eb3c005ce8a0545
Unai Santa <unai.santamariape@elorrieta-errekamari.com>
2021-01-27 15:22:56 +0100

    correción mensaje de error al no encontrar las páginas

--- RETO3/app.js
+++ RETO3/app.js
@@ -3,13 +3,13 @@ const app = express();
 const path = require('path');
 const puerto = 3000;
 
-app.use(express.static('public',{extensions:['html']}));
+app.use(express.static(path.join(__dirname, "public"),{extensions:['html']}));
 
 // Error index
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '//public//index.html'),function(error){
     if(error){
-      res.status(404).send("<h1>error 404: Archivo index.html no encontrado.</h1>");
+      res.status(404).send('<h1>error 404: Archivo "index.html" no encontrado.</h1>');
     }
   });
 });
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 app.get('/bares', (req, res) => {
   res.sendFile(path.join(__dirname + '//public//bares.html'),function(error){
     if(error){
-      res.status(404).send('<h1>error 404: No existe o no se encuentra el archivo "bares.html"</h1>');
+      res.status(404).send('<h1>error 404: Archivo "bares.html" no encontrado."</h1>');
     }
 
   });
@@ -27,7 +27,7 @@ app.get('/bares', (req, res) => {
 app.get('/restaurantes', (req, res) => {
 	  res.sendFile(path.join(__dirname + '//public//restaurantes.html'),function(error){
 	    if(error){
-	      res.status(404).send('<h1>error 404: No existe o no se encuentra el archivo "restaurantes.html"</h1>');
+	      res.status(404).send('<h1>error 404: Archivo "restaurantes.html" no encontrado."</h1>');
 	    }
 
 	  });
@@ -38,7 +38,7 @@ app.get('/restaurantes', (req, res) => {
 app.get('/cafeterias', (req, res) => {
   res.sendFile(path.join(__dirname + '//public//cafeterias.html'),function(error){
     if(error){
-      res.status(404).send('<h1>error 404: No existe o no se encuentra el archivo "cafeterias.html"</h1>');
+      res.status(404).send('<h1>error 404: Archivo "cafeterias.html" no encontrado."</h1>');
     }
 
   });
