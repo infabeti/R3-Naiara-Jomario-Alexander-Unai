b57035e4f3caabcbea17b99c7d82706af4c7b66a
unaisantamariape <unai.santamariape@elorrieta-errekamari.com>
2021-02-03 17:19:57 +0100

    llamada a Api según el div en el que se haya clickado el boton de menu

--- RETO3/public/js/index.js
+++ RETO3/public/js/index.js
@@ -5,7 +5,29 @@ function inicio(){
 	iniciarDialogo();
 	
 	mapa();
- 
+/* MOSTRAR MENÚ */
+$('.botonMenu').click(function (){
+var divMenu=$(this).next().attr("id");
+	alert(divMenu);
+	var num = divMenu.substr(7);
+	alert(num);
+	 var url = ('http://localhost:8080/menu'+num);
+	 console.log(url);
+	$('#'+divMenu).toggle("slow");
+	fetch(url,{
+		
+	}).then((response) => response.json())
+	.then(data =>{
+		for(var i=0; i<4; i++){
+			// console.log("aqui");
+			   $("#"+divMenu+" #plato"+(i+1)).text(data.platos[i]);
+			   $("#"+divMenu+" #bebida"+(i+1)).text(data.bebidas[i]);
+			   $("#"+divMenu+" #snacks"+(i+1)).text(data.snacks[i]);
+		}
+	});
+
+});
+/******************************/
 }
 function mapa(){
 	$(".boton").click(function abrirDialogo(){
@@ -88,11 +110,6 @@ function buscarDatos(id){
 	});
 }
 
-function mostrarMenu(div){
-
-	$('#menubar'+div).toggle("slow");
-	menus();
-}
 function buscarIds(){
 		var numItems = $('.nombreBar').length;
 		var min = numItems;
@@ -114,16 +131,3 @@ function buscarIds(){
 // };
 
 
-function menus(){
-	
-	
-	fetch('http://localhost:8080/menu',{
-		
-	}).then((response) => response.json())
-	.then(data =>{
-		for(var i=0; i<4; i++){
-			// console.log("aqui");
-			   $("#plato"+(i+1)).text(data.platos[i]);
-		}
-	});
-}
